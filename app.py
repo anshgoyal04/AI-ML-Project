52eb26a2fff2ec60f2611700e47cd85c058c47bd
anshgoyal04 <96487283+anshgoyal04@users.noreply.github.com>
2025-04-09 12:56:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-import streamlit as st
+from dotenv import load_dotenv
+import os
 import openai
+import streamlit as st
 
-# Replace with your actual OpenAI API Key (You can also use a .env file to load it securely)
-openai.api_key = "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"
+# Load API key from .env
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 st.set_page_config(page_title="Hiring Assistant Chatbot", layout="centered")
 st.title("🤖 TalentScout - AI Hiring Assistant")
